7e61fe5319742e94127958d3b86e924f3eafa9b4
David Banham <david@banham.id.au>
2013-08-20 19:23:29 +1000

    path?

--- mkdir.js
+++ mkdir.js
@@ -1,9 +1,17 @@
 fs = require('fs')
+path = require('path');
 fs.mkdir('./test', function(err) {
-  if(err)
+  if(err) {
     throw err
+  }
 });
 fs.writeFile('./testfile', 'oh hey, travis', function(err) {
-  if(err)
+  if(err) {
     throw err
+  }
+});
+fs.mkdir(path.resolve('./hmm', function(err) {
+  if(err) {
+    throw err
+  }
 });
